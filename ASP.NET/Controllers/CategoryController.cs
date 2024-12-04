21290b0ff52ea5e73b595b3b9ec886fccf1c6a64
longthinh <longthinhdev@icloud.com>
2024-12-04 16:01:26 +0700

    Updates

---
+++ ASP.NET/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ASP.NET.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult AllCategory()
+        {
+            return View();
+        }
+
+        public ActionResult ProductByCategory()
+        {
+            return View();
+        }
+
+        public ActionResult ListCategory()
+        {
+            return View();
+        }
+
+        public ActionResult GridCategory()
+        {
+            return View();
+        }
+    }
+}
