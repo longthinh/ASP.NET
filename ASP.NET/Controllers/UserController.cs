c5f2f1e0dc0f318010a7c45e40cdbea022fddfb8
longthinh <longthinhdev@icloud.com>
2024-12-17 17:14:20 +0700

    Update 3

--- ASP.NET/Controllers/UserController.cs
+++ ASP.NET/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ASP.NET.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,12 +12,35 @@ namespace ASP.NET.Controllers
         // GET: User
         public ActionResult Login()
         {
+            ViewBag.ErrorMessage = null;
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Login(string username, string password)
+        {
+            // Kiểm tra thông tin đăng nhập
+            if (username == "sa" && password == "sa")
+            {
+                // Chuyển hướng về trang chủ khi đăng nhập thành công
+                return RedirectToAction("Index", "Home");
+            }
+
+            // Gửi thông báo lỗi khi đăng nhập thất bại
+            ViewBag.ErrorMessage = "Tên đăng nhập hoặc mật khẩu không chính xác";
             return View();
         }
 
         public ActionResult Register()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult Register(User objSubject)
+        {
+            return View(objSubject);
         }
     }
 }
